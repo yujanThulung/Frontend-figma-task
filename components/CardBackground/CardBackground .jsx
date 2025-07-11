66446d3a078106cb63df7dc7683b5b249b038bdd
yujan Thulung <yujanrai07@gmail.com>
2025-07-11 22:00:20 +0545

    Background created

--- components/CardBackground/CardBackground .jsx
+++ components/CardBackground/CardBackground .jsx
@@ -4,11 +4,13 @@ import CircleButton from "../CircleButton/CircleButton";
 
 const CardBackground = ({ profileImage, vectorImage }) => {
     return (
-        <div className="relative w-[600px] h-[340px] bg-[#F85A5A] rounded-3xl flex items-center justify-between px-6 py-4">
-            <div className="absolute left-4 top-1/2 -translate-y-1/2 z-10">
-                <div className="relative w-20 h-20">
-                    <div className="absolute inset-0 bg-white rounded-full z-0 -translate-x-14" />
-                    <CircleButton direction="left" />
+        <div className="relative w-[600px] h-[340px] bg-[#F85A5A] rounded-3xl flex items-center justify-between px-6 py-4 overflow-visible">
+            <div className="absolute -left-2 top-1/2 -translate-y-1/2 z-10">
+                <div className="relative w-24 h-24">
+                    <div className="absolute left-3 top-0 w-20 h-20 bg-white rounded-r-full transform -translate-x-3" />
+                    <div className="absolute left-0 top-0 w-20 h-20 flex items-center justify-center">
+                        <CircleButton direction="left" />
+                    </div>
                 </div>
             </div>
 
@@ -28,12 +30,12 @@ const CardBackground = ({ profileImage, vectorImage }) => {
                 <CardText />
             </div>
 
-            <div className="absolute right-4 top-1/2 -translate-y-1/2 z-10">
-                <div className="relative w-26 h-22" dir="rtl">
-                    <div className="absolute inset-0 bg-white  rounded-l-full right-9 z-0 translate-x-16" />
-                    <div></div>
-                    <div className="absolute inset-0 bg-gray-800 rounded-s-full z-0 left-[20%]  top-[20%] translate-x-[22%] -translate-y-[95%]" />
-                    <CircleButton direction="right" />
+            <div className="absolute -right-2 top-1/2 -translate-y-1/2 z-10">
+                <div className="relative w-24 h-24">
+                    <div className="absolute right-3 top-0 w-20 h-20 bg-white rounded-l-full transform translate-x-3" />
+                    <div className="absolute right-0 top-0 w-20 h-20 flex items-center justify-center">
+                        <CircleButton direction="right" />
+                    </div>
                 </div>
             </div>
         </div>
